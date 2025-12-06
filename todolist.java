00c635b71b78038703aeb478eb0ca9cedcd646f6
Rabiah Mukhtar <rabiahmukhtar198@gmail.com>
2025-12-06 20:26:30 +0530

    add Arraylist to store tasks and basic menu options

--- todolist.java
+++ todolist.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 
 public class todolist {
     public static void main(String[] args) {
+        ArrayList<String> todoList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to your To-Do List!");
+        while(true) {
+            System.out.println("Choose an option: 1.Add Task 2.View Tasks 3.Remove Task 4.Exit");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+        }
     }
 
     
