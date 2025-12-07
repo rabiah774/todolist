1fc991789a27fa8e166ba90392152b836fa90023
Rabiah Mukhtar <rabiahmukhtar198@gmail.com>
2025-12-07 16:19:51 +0530

    Added feature of add task and view task

--- todolist.java
+++ todolist.java
@@ -9,9 +9,25 @@ public class todolist {
         while(true) {
             System.out.println("Choose an option: 1.Add Task 2.View Tasks 3.Remove Task 4.Exit");
             int choice = scanner.nextInt();
-            scanner.nextLine(); // Consume newline
+            scanner.nextLine(); 
+            switch(choice) {
+                case 1:
+                    System.out.println("Enter the task to add:");
+                    String taskToAdd = scanner.nextLine();
+                    todoList.add(taskToAdd);
+                    System.out.println("Task added.");
+                    break;
+                case 2:
+                    System.out.println("Your To-Do List:");
+                    for (int i = 0; i < todoList.size(); i++) {
+                        System.out.println((i + 1) + ". " + todoList.get(i));
+                    }
+                    break;
+              
+
+                     }
         }
     }
-
-    
 }
+
+
